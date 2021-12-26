28090ef5f1669cc64fc51a067de54b66f92a30d8
KushagraAggarwal <Kushagra.8oct@gmail.com>
2021-12-26 17:02:10 +0530

    title input added

--- js/app.js
+++ js/app.js
@@ -4,18 +4,25 @@ showNotes();
 
 let addBtn = document.getElementById("addBtn");
 
+
 addBtn.addEventListener("click", () => {
   let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
-  notesObj.push(addTxt.value);
+  let myObj = {
+    title:addTitle.value,
+    text:addTxt.value
+  }
+  notesObj.push(myObj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
-  console.log(notesObj);
+  addTitle.value="";
+  // console.log(notesObj);
   showNotes();
 });
 
@@ -31,8 +38,8 @@ function showNotes() {
     html += `
         <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
           <div class="card-body">
-            <h5 class="card-title">your Note ${index + 1} </h5>
-            <p class="card-text">${element}</p>
+            <h5 class="card-title">${element.title} </h5>
+            <p class="card-text">${element.text}</p>
             <button id=${index} onclick=deleteNode(this.id) class="btn btn-primary">Delete Note</button>
           </div>
         </div>`;
@@ -47,7 +54,7 @@ function showNotes() {
 }
 
 function deleteNode(index) {
-  console.log("deleteing the " + index + " index");
+  // console.log("deleteing the " + index + " index");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
